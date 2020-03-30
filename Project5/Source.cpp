01eefb5663249e026eb583c8c1747ea8081ffe10
alsy1311 <alsy1311@yandex.ru>
2020-03-31 00:42:41 +0300

    .

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -13,13 +13,11 @@ void view_directory(const std::experimental::filesystem::path& path)
 		{
 			auto file_name = entry.path().filename().string();
 			std::cout << file_name << std::endl;
+			auto file_type = entry.path().extension().string();
+			std::cout << file_type << std::endl;
 		}
 	}
-	auto ftime = std::experimental::filesystem::last_write_time(path); // не знаю откуда взять путь последнего измененного файла 
-	auto file_name2 = path.filename().string();
-	std::cout << file_name2 << std::endl;
-	auto file_type = std::experimental::filesystem::file_type();// как файл тайп вызвать и как вывести 
-	//std::cout << file_type << std::endl;
+	auto ftime = std::experimental::filesystem::last_write_time(path);
 	std::time_t cftime = decltype (ftime) ::clock::to_time_t(ftime);
 #pragma warning(suppress : 4996)
 	std::cout << "Time of changes " << std::asctime(std::localtime(&cftime)) << ' \ n ';
